0dcec569206ff4d07c3e4951714cc0683b629b8d
Karl Hänsel <Karl-Haensel@gmx.de>
2026-06-12 18:39:18 +0200

    Add basic app setup for first deployment as a test.

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+"""Main entry point for streamlit app 'choirmatch'."""
+
+import streamlit as st
+
+st.header("ChoirMatch - plan your rehearsals and performances based on choir feedback!")
+st.write("< currently in development, coming soon >")
