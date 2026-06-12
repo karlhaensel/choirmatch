581a9f08c512d38da51f374b63c99af9ac0f4fba
Karl Hänsel <Karl-Haensel@gmx.de>
2026-06-12 19:09:01 +0200

    Add CSV file input and display raw data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 """Main entry point for streamlit app 'choirmatch'."""
 
+import pandas as pd
 import streamlit as st
 
-st.header("ChoirMatch - plan your rehearsals and performances based on choir feedback!")
-st.write("< currently in development, coming soon >")
+st.title(":green[ChoirMatch]")
+
+st.sidebar.header("Input and options")
+input_file = st.sidebar.file_uploader(
+    "Upload a CSV file containing choir availability feedback.",
+    type=["csv"],
+    help="CSV file should have three columns for name, voice, and comment "
+    "(comment can be empty, others not). "
+    "All other columns are considered date columns to chose from later on.",
+)
+if input_file is None:
+    st.info("Please upload a CSV file in the sidebar to the left.")
+    st.stop()
+
+df = pd.read_csv(input_file)
+
+st.header("Availability data")
+st.dataframe(df)
